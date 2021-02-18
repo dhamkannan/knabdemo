64d824d26add80a94f2df9af40feaf8ab5a3a48f
Dhamodharakannan Vellaichamy <vellaichamy.v@pg.com>
2021-02-18 03:57:33 +0100

    Initial Commit

---
+++ terraform/instance.tf
@@ -0,0 +1,89 @@
+##################################################################################
+# VARIABLES
+##################################################################################
+
+variable "key_name" {}
+
+
+##################################################################################
+# DATA
+##################################################################################
+
+
+data "aws_ami" "centos" {
+  owners      = ["679593333241"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["CentOS Linux 7 x86_64 HVM EBS *"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
+
+##################################################################################
+# RESOURCES
+##################################################################################
+
+
+# SECURITY GROUPS #
+# Nginx security group 
+resource "aws_security_group" "nginx-sg" {
+  name   = "nginx_sg"
+  vpc_id = aws_vpc.vpc.id
+
+  # SSH access from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# INSTANCES #
+resource "aws_instance" "nginx1" {
+  ami                    = data.aws_ami.centos.id
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.subnet1.id
+  vpc_security_group_ids = [aws_security_group.nginx-sg.id]
+  key_name               = var.key_name
+
+}
+
+resource "local_file" "server_name" {
+    filename = "../ansible-playbooks/inventory"
+    content  = <<-EOT
+      [docker-hosts]
+      ${aws_instance.nginx1.public_dns}
+    EOT
+}
+##################################################################################
+# OUTPUT
+##################################################################################
+
+output "aws_instance_public_dns" {
+  value = aws_instance.nginx1.public_dns
+}
---
+++ terraform/vpc.tf
@@ -0,0 +1,56 @@
+##################################################################################
+# VARIABLES
+##################################################################################
+
+variable "network_address_space" {
+  default = "10.1.0.0/16"
+}
+variable "subnet1_address_space" {
+  default = "10.1.0.0/24"
+}
+
+##################################################################################
+# DATA
+##################################################################################
+
+data "aws_availability_zones" "available" {}
+
+
+##################################################################################
+# RESOURCES
+##################################################################################
+
+# NETWORKING #
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.network_address_space
+  enable_dns_hostnames = "true"
+
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+}
+
+resource "aws_subnet" "subnet1" {
+  cidr_block              = var.subnet1_address_space
+  vpc_id                  = aws_vpc.vpc.id
+  map_public_ip_on_launch = "true"
+  availability_zone       = data.aws_availability_zones.available.names[0]
+
+}
+
+# ROUTING #
+resource "aws_route_table" "rtb" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "rta-subnet1" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.rtb.id
+}
